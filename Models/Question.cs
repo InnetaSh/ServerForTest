3429e86b5b15706a90565765429e568d3d02a32e
InnetaSh <inneta3@gmail.com>
2025-03-27 11:37:15 +0200

    fix models

---
+++ Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerForTest.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public String? QuestionText { get; set; }
+        public int Weight {get; set; }
+
+        public String? ImagePath { get; set; }
+        public bool IsMultiAnswers { get; set; }
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+    }
+}
